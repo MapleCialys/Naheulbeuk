813e9fbc97a79f7d7190fda1c00e1dd2acf32550
Jean-Philippe Hoareau Méavilla <jphm1992@hotmail.fr>
2024-05-07 14:15:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,19 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+// Utilisation de bodyParser avant le routage
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
+// Utilisation de bodyParser avant le routage
+newgameRoute.use(express.urlencoded({ extended: true }));
 app.use('/newgame', newgameRoute);
 app.use('/page', pageRoute);
-newgameRoute.use(express.urlencoded({extended: true}));
 
-app.get('/', (req,res) =>
-{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(port, function () {
-    console.log('app is running...');
+app.listen(port, () => {
+    console.log('App is running...');
 });
